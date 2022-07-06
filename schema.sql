9aedf0e817a17ae9bb88984b9707fd33765364d7
dmcneill0711_273783 <dmcneill0711@hotmail.com>
2022-07-06 18:43:04 -0400

    update SQL code

--- schema.sql
+++ schema.sql
@@ -192,5 +192,221 @@ DROP COLUMN miscarriagestotal,
 DROP COLUMN versiondate
 ;
 
+-- Update State Names In abortion_info Table To Match State Abbreviations In national_pregnancy Table
+
+UPDATE abortion_info
+SET state = 'WY'
+Where id = '1';
+
+UPDATE abortion_info
+SET state = 'SD'
+Where id = '2';
+
+UPDATE abortion_info
+SET state = 'KY'
+Where id = '3';
+
+UPDATE abortion_info
+SET state = 'ID'
+Where id = '4';
+
+UPDATE abortion_info
+SET state = 'MO'
+Where id = '5';
+
+UPDATE abortion_info
+SET state = 'MS'
+Where id = '6';
+
+UPDATE abortion_info
+SET state = 'WV'
+Where id = '7';
+
+UPDATE abortion_info
+SET state = 'UT'
+Where id = '8';
+
+UPDATE abortion_info
+SET state = 'SC'
+Where id = '9';
+
+UPDATE abortion_info
+SET state = 'NE'
+Where id = '10';
+
+UPDATE abortion_info
+SET state = 'AR'
+Where id = '11';
+
+UPDATE abortion_info
+SET state = 'WI'
+Where id = '12';
+
+UPDATE abortion_info
+SET state = 'IN'
+Where id = '13';
+
+UPDATE abortion_info
+SET state = 'OK'
+Where id = '14';
+
+UPDATE abortion_info
+SET state = 'IA'
+Where id = '15';
+
+UPDATE abortion_info
+SET state = 'AL'
+Where id = '16';
+
+UPDATE abortion_info
+SET state = 'ND'
+Where id = '17';
+
+UPDATE abortion_info
+SET state = 'MT'
+Where id = '18';
+
+UPDATE abortion_info
+SET state = 'AK'
+Where id = '19';
+
+UPDATE abortion_info
+SET state = 'ME'
+Where id = '20';
+
+UPDATE abortion_info
+SET state = 'TN'
+Where id = '21';
+
+UPDATE abortion_info
+SET state = 'NH'
+Where id = '22';
+
+UPDATE abortion_info
+SET state = 'AZ'
+Where id = '23';
+
+UPDATE abortion_info
+SET state = 'TX'
+Where id = '24';
+
+UPDATE abortion_info
+SET state = 'OH'
+Where id = '25';
+
+UPDATE abortion_info
+SET state = 'MN'
+Where id = '26';
+
+UPDATE abortion_info
+SET state = 'VA'
+Where id = '27';
+
+UPDATE abortion_info
+SET state = 'DE'
+Where id = '28';
+
+UPDATE abortion_info
+SET state = 'LA'
+Where id = '29';
+
+UPDATE abortion_info
+SET state = 'CO'
+Where id = '30';
+
+UPDATE abortion_info
+SET state = 'VT'
+Where id = '31';
+
+UPDATE abortion_info
+SET state = 'NM'
+Where id = '32';
+
+UPDATE abortion_info
+SET state = 'OR'
+Where id = '33';
+
+UPDATE abortion_info
+SET state = 'HI'
+Where id = '34';
+
+UPDATE abortion_info
+SET state = 'WA'
+Where id = '35';
+
+UPDATE abortion_info
+SET state = 'KS'
+Where id = '36';
+
+UPDATE abortion_info
+SET state = 'PA'
+Where id = '37';
+
+UPDATE abortion_info
+SET state = 'MA'
+Where id = '38';
+
+UPDATE abortion_info
+SET state = 'MI'
+Where id = '39';
+
+UPDATE abortion_info
+SET state = 'NC'
+Where id = '40';
+
+UPDATE abortion_info
+SET state = 'NV'
+Where id = '41';
+
+UPDATE abortion_info
+SET state = 'CA'
+Where id = '42';
+
+UPDATE abortion_info
+SET state = 'IL'
+Where id = '43';
+
+UPDATE abortion_info
+SET state = 'RI'
+Where id = '44';
+
+UPDATE abortion_info
+SET state = 'GA'
+Where id = '45';
+
+UPDATE abortion_info
+SET state = 'CT'
+Where id = '46';
+
+UPDATE abortion_info
+SET state = 'FL'
+Where id = '47';
+
+UPDATE abortion_info
+SET state = 'MD'
+Where id = '48';
+
+UPDATE abortion_info
+SET state = 'NY'
+Where id = '49';
+
+UPDATE abortion_info
+SET state = 'NJ'
+Where id = '50';
+
 -- Queries Run
 
+-- To Get Any Information From Each State Based On Year:
+
+SELECT * 
+FROM national_pregnancy
+WHERE year = 'year';
+
+-- (years available: 1988, 1992, 1996, 2000, 2005-2017)
+
+-- To Get Any Information From 2017 (The Latest Year)
+
+SELECT *
+FROM national_pregnancy
+INNER JOIN abortion_info
+    ON national_pregnancy.state = abortion_info.state
